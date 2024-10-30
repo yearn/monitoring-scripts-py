f862f3c22f098679a199187fb4a1bb9c320bb9f2
spalen0 <spalen@proton.me>
2024-10-30 13:10:37 +0100

    chore: create cache module

---
+++ utils/cache.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+filename = os.getenv("FILENAME", "cache-id.txt")
+
+
+# TODO: extract these 2 functions to a common file
+def get_last_queued_id_from_file(protocol):
+    if not os.path.exists(filename):
+        return 0
+    else:
+        with open(filename, "r") as f:
+            # read line by line in format "protocol:proposal_id"
+            lines = f.readlines()
+            for line in lines:
+                protocol_name, proposal_id = line.strip().split(":")
+                if protocol_name == protocol:
+                    return int(proposal_id)
+    return 0
+
+
+def write_last_queued_id_to_file(protocol, last_id):
+    # check if the proposal ud is already in the file, then update the id else append
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            lines = f.readlines()
+            for i, line in enumerate(lines):
+                protocol_name, _ = line.strip().split(":")
+                if protocol_name == protocol:
+                    lines[i] = f"{protocol}:{last_id}\n"
+                    break
+            else:
+                lines.append(f"{protocol}:{last_id}\n")
+        with open(filename, "w") as f:
+            f.writelines(lines)
+    else:
+        lines = [f"{protocol}:{last_id}\n"]
+        with open(filename, "w") as f:
+            f.writelines(lines)
