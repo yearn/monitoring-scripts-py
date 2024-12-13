20e10076b8a0859bb727c83634aa5547882c0194
spalen0 <spalen@proton.me>
2024-12-13 14:04:33 +0700

    chore: extract cf value

--- usd0/main.py
+++ usd0/main.py
@@ -6,13 +6,14 @@ from utils.telegram import send_telegram_message
 load_dotenv()
 dune = DuneClient(os.getenv("DUNE_API_KEY"))
 PROTOCOL = "USD0"
+COLLATERAL_FACTOR_MINIMUM = 100.6
 
 
 def query_cf():
     query_result = dune.get_latest_result(3886520)
     newest_data = query_result.result.rows[0]
     collateral_factor = newest_data["collateral_factor"]
-    if collateral_factor < 1.1:
+    if collateral_factor < COLLATERAL_FACTOR_MINIMUM:
         # Collateral factor has fallen below accept risk
         message = f"USD0 collateral factor is {collateral_factor}"
         send_telegram_message(message, PROTOCOL)
