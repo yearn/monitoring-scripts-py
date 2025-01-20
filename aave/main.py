ab2810d9c7b02457183992bb5e3f16bb470a75d4
spalen0 <spalen@proton.me>
2025-01-20 16:08:22 +0700

    fix: aave usdc.e atoken address

--- aave/main.py
+++ aave/main.py
@@ -21,48 +21,31 @@ def load_abi(file_path):
 
 ABI_ATOKEN = load_abi("aave/abi/AToken.json")
 
-# Map addresses by chain
+# Map addresses and symbols by chain
 ADDRESSES_BY_CHAIN = {
     Chain.POLYGON: [
-        "0x1a13F4Ca1d028320A707D99520AbFefca3998b7F",  # ausdc.e
-        "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",  # usdc.e
-        "0xA4D94019934D8333Ef880ABFFbF2FDd611C762BD",  # ausdc
-        "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",  # usdc
-        "0x6ab707Aca953eDAeFBc4fD23bA73294241490620",  # ausdt
-        "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",  # usdt
-        "0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE",  # adai
-        "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",  # dai
-        "0x6d80113e533a2C0fe82EaBD35f1875DcEA89Ea97",  # amatic
-        "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",  # matic
-        "0xe50fA9b3c56FfB159cB0FCA61F5c9D750e8128c8",  # aweth
-        "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",  # weth
-        # Add more pairs as needed
+        # aToken, underlying, symbol
+        ("0x625E7708f30cA75bfd92586e17077590C60eb4cD", "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", "USDC.e"),
+        ("0xA4D94019934D8333Ef880ABFFbF2FDd611C762BD", "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359", "USDC"),
+        ("0x6ab707Aca953eDAeFBc4fD23bA73294241490620", "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", "USDT"),
+        ("0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE", "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063", "DAI"),
+        ("0x6d80113e533a2C0fe82EaBD35f1875DcEA89Ea97", "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", "MATIC"),
+        ("0xe50fA9b3c56FfB159cB0FCA61F5c9D750e8128c8", "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", "WETH"),
     ],
     Chain.MAINNET: [
-        "0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8",  # aweth
-        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",  # weth
-        "0x23878914EFE38d27C4D67Ab83ed1b93A74D4086a",  # ausdt
-        "0xdAC17F958D2ee523a2206206994597C13D831ec7",  # usdt
-        "0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c",  # ausdc
-        "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",  # usdc
-        "0x018008bfb33d285247A21d44E50697654f754e63",  # adai
-        "0x6B175474E89094C44Da98b954EedeAC495271d0F",  # dai
-        "0xb82fa9f31612989525992FCfBB09AB22Eff5c85A",  # acrvUSD
-        "0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E",  # crvUSD
+        ("0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", "WETH"),
+        ("0x23878914EFE38d27C4D67Ab83ed1b93A74D4086a", "0xdAC17F958D2ee523a2206206994597C13D831ec7", "USDT"),
+        ("0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "USDC"),
+        ("0x018008bfb33d285247A21d44E50697654f754e63", "0x6B175474E89094C44Da98b954EedeAC495271d0F", "DAI"),
+        ("0xb82fa9f31612989525992FCfBB09AB22Eff5c85A", "0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E", "crvUSD"),
     ],
     Chain.ARBITRUM: [
-        "0xe50fA9b3c56FfB159cB0FCA61F5c9D750e8128c8",  # aweth
-        "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",  # weth
-        "0x724dc807b04555b71ed48a6896b6F41593b8C637",  # ausdc
-        "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",  # usdc
-        "0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE",  # adai
-        "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",  # dai
-        "0x625E7708f30cA75bfd92586e17077590C60eb4cD",  # ausdc.e
-        "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",  # usdc.e
-        "0x6ab707Aca953eDAeFBc4fD23bA73294241490620",  # ausdt
-        "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",  # usdt
-        "0x6533afac2E7BCCB20dca161449A13A32D391fb00",  # aarb
-        "0x912CE59144191C1204E64559FE8253a0e49E6548",  # arb
+        ("0xe50fA9b3c56FfB159cB0FCA61F5c9D750e8128c8", "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", "WETH"),
+        ("0x724dc807b04555b71ed48a6896b6F41593b8C637", "0xaf88d065e77c8cC2239327C5EDb3A432268e5831", "USDC"),
+        ("0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE", "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", "DAI"),
+        ("0x625E7708f30cA75bfd92586e17077590C60eb4cD", "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", "USDC.e"),
+        ("0x6ab707Aca953eDAeFBc4fD23bA73294241490620", "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9", "USDT"),
+        ("0x6533afac2E7BCCB20dca161449A13A32D391fb00", "0x912CE59144191C1204E64559FE8253a0e49E6548", "ARB"),
     ],
     # We don't use optimism - add if needed
     # Chain.OPTIMISM: [
@@ -106,10 +89,7 @@ def process_assets(chain: Chain):
     # Prepare all contracts and batch calls
     contracts = []
     with client.batch_requests() as batch:
-        for i in range(0, len(addresses), 2):
-            atoken_address = addresses[i]
-            underlying_token_address = addresses[i + 1]
-
+        for atoken_address, underlying_token_address, token_symbol in addresses:
             atoken = client.eth.contract(address=atoken_address, abi=ABI_ATOKEN)
             underlying_token = client.eth.contract(
                 address=underlying_token_address, abi=ABI_ATOKEN
@@ -118,30 +98,28 @@ def process_assets(chain: Chain):
 
             batch.add(atoken.functions.totalSupply())
             batch.add(underlying_token.functions.balanceOf(atoken_address))
-            batch.add(underlying_token.functions.symbol())
 
         responses = client.execute_batch(batch)
-        num_pairs = len(addresses) / 2
-        expected_responses = num_pairs * 3
+        num_pairs = len(addresses)
+        expected_responses = num_pairs * 2  # Now only 2 calls per token pair
         if len(responses) != expected_responses:
             raise ValueError(
                 f"Expected {expected_responses} responses from batch, got: {len(responses)}"
             )
 
     # Process results
-    for i in range(0, len(responses), 3):
-        ts = responses[i]  # totalSupply
-        av = responses[i + 1]  # balanceOf
-        token_name = responses[i + 2]  # symbol
+    for i, (_, _, token_symbol) in enumerate(addresses):
+        ts = responses[i * 2]  # totalSupply
+        av = responses[i * 2 + 1]  # balanceOf
 
         debt = ts - av
         ur = debt / ts if ts != 0 else 0
 
-        print_stuff(chain.name, token_name, ur)
+        print_stuff(chain.name, token_symbol, ur)
 
 
 def main():
-    for chain in [Chain.POLYGON, Chain.MAINNET, Chain.ARBITRUM]:
+    for chain in [Chain.MAINNET, Chain.POLYGON, Chain.ARBITRUM]:
         print(f"Processing {chain.name} assets...")
         process_assets(chain)
 
