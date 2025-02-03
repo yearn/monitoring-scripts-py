12b798a278b68a8eea908033fc808e6f8fd8385d
spalen0 <spalen@proton.me>
2025-02-03 12:43:34 +0100

    chore: low morpho utilization alert

--- morpho/markets.py
+++ morpho/markets.py
@@ -7,7 +7,7 @@ API_URL = "https://blue-api.morpho.org/graphql"
 MORPHO_URL = "https://app.morpho.org"
 PROTOCOL = "MORPHO"
 BAD_DEBT_RATIO = 0.1
-LIQUIDITY_THRESHOLD = 0.10  # 10% of total assets # TODO: think about lowering this to 0.05 = 5% of total assets
+LIQUIDITY_THRESHOLD = 0.05  # 5% of total assets
 
 # Map vaults by chain
 VAULTS_BY_CHAIN = {
