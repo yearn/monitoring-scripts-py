7bc45268334dc8cb4ee07e04d1cada07b7ad8416
spalen0 <spalen@proton.me>
2025-02-25 15:06:26 +0100

    chore: update morpho score

--- morpho/markets.py
+++ morpho/markets.py
@@ -35,8 +35,8 @@ VAULTS_BY_CHAIN = {
             "0x543257eF2161176D7C8cD90BA65C2d4CaEF5a796",
             3,
         ],
-        ["Seamless/Gauntlet WETH", "0x27D8c7273fd3fcC6956a0B370cE5Fd4A7fc65c18", 2],
         ["Seamless/Gauntlet USDC", "0x616a4E1db48e22028f6bbf20444Cd3b8e3273738", 2],
+        ["Seamless/Gauntlet WETH", "0x27D8c7273fd3fcC6956a0B370cE5Fd4A7fc65c18", 3],
         ["Seamless/Gauntlet cbBTC", "0x5a47C803488FE2BB0A0EAaf346b420e4dF22F3C7", 3],
     ],
 }
