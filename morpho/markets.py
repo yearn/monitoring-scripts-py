d174f99b2c3d24cbf5d0b9d29a3466c54f0efb32
spalen0 <spalen@proton.me>
2025-11-05 12:45:58 +0100

    chore: steakhouse high yield as yv

--- morpho/markets.py
+++ morpho/markets.py
@@ -20,7 +20,7 @@ MORPHO_URL = "https://app.morpho.org"
 PROTOCOL = "MORPHO"
 BAD_DEBT_RATIO = 0.005  # 0.5% of total borrowed tvl
 LIQUIDITY_THRESHOLD = 0.01  # 1% of total assets
-LIQUIDITY_THRESHOLD_YV_COLLATERAL = 0.15  # 15% of total assets
+LIQUIDITY_THRESHOLD_YV_COLLATERAL = 0.125  # 12.5% of total assets
 
 # Map vaults by chain
 VAULTS_BY_CHAIN = {
@@ -68,7 +68,7 @@ VAULTS_BY_CHAIN = {
         ["Yearn OG USDT", "0x8ED68f91AfbE5871dCE31ae007a936ebE8511d47", 1],
         ["Yearn OG WBTC", "0xe107cCdeb8e20E499545C813f98Cc90619b29859", 1],
         ["Gauntlet USDC", "0xE4248e2105508FcBad3fe95691551d1AF14015f7", 2],
-        ["SteakhouseHigh Yield USDC", "0x1445A01a57D7B7663CfD7B4EE0a8Ec03B379aabD", 2],
+        ["SteakhouseHigh Yield USDC", "0x1445A01a57D7B7663CfD7B4EE0a8Ec03B379aabD", 1],
         ["Gauntlet USDT", "0x1ecDC3F2B5E90bfB55fF45a7476FF98A8957388E", 1],
         ["SteakhousePrime USDC", "0x61D4F9D3797BA4dA152238c53a6f93Fb665C3c1d", 1],
         ["Gauntlet WETH", "0xC5e7AB07030305fc925175b25B93b285d40dCdFf", 1],
@@ -86,6 +86,7 @@ VAULTS_WITH_YV_COLLATERAL_BY_ASSET = {
         "0x203A662b0BD271A6ed5a60EdFbd04bFce608FD36": [  # USDC asset address on Katana
             ["Yearn OG USDC", "0xCE2b8e464Fc7b5E58710C24b7e5EBFB6027f29D7"],
             ["SteakhousePrime USDC", "0x61D4F9D3797BA4dA152238c53a6f93Fb665C3c1d"],
+            ["SteakhouseHigh Yield USDC", "0x1445A01a57D7B7663CfD7B4EE0a8Ec03B379aabD"],
             ["Gauntlet USDC", "0xE4248e2105508FcBad3fe95691551d1AF14015f7"],
         ],
         # USDT vaults - using addresses from VAULTS_BY_CHAIN as source of truth
